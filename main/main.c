652fda1e6d85057b31c5cd0f72024e2d72f00a99
Mohammad Sadequr Rahman <sadequr.rahman.rabby@gmail.com>
2022-04-17 19:29:06 +0300

    adopt callback parameter type

--- main/main.c
+++ main/main.c
@@ -12,35 +12,44 @@
 
 
 
-uint8_t SERVICE_UUID[] = {0x12, 0x02};//{0x00, 0x00, 0x00, 0x01, 0x10, 0x00, 0x20, 0x00, 0x30, 0x00, 0x11, 0x11, 0x22, 0x22, 0x33, 0x33};
-uint8_t CHAR_UUID[] = {0x12, 0x01};//{0x00, 0x00, 0x00, 0x02, 0x10, 0x00, 0x20, 0x00, 0x30, 0x00, 0x11, 0x11, 0x22, 0x22, 0x33, 0x33};
-uint8_t CHAR_UUID1[] = {0x12, 0x02};//{0x00, 0x00, 0x00, 0x03, 0x10, 0x00, 0x20, 0x00, 0x30, 0x00, 0x11, 0x11, 0x22, 0x22, 0x33, 0x33};
+uint8_t SERVICE_UUID[] = {0x00, 0x12};//{0x00, 0x00, 0x00, 0x01, 0x10, 0x00, 0x20, 0x00, 0x30, 0x00, 0x11, 0x11, 0x22, 0x22, 0x33, 0x33};
+uint8_t CHAR_UUID[] = {0x01, 0x12};//{0x00, 0x00, 0x00, 0x02, 0x10, 0x00, 0x20, 0x00, 0x30, 0x00, 0x11, 0x11, 0x22, 0x22, 0x33, 0x33};
+uint8_t CHAR_UUID1[] = {0x02, 0x12};//{0x00, 0x00, 0x00, 0x03, 0x10, 0x00, 0x20, 0x00, 0x30, 0x00, 0x11, 0x11, 0x22, 0x22, 0x33, 0x33};
+uint8_t CHAR_UUID2[] = {0x03, 0x12};
 uint16_t DES_UUID =  0x2902;//{0x00, 0xA0, 0x01, 0x02, 0x10, 0x00, 0x20, 0x00, 0x30, 0x00, 0x11, 0x11, 0x22, 0x22, 0x33, 0x33};
 static const char *TAG = "main";
 ble_profile_t* profile = (void*)0;
 bleDevice_config_t *conf = (void*)0;
-ble_char_t* ch = NULL; 
-ble_char_t* ch1 = NULL;
-ble_descrp_t* des = NULL;
-uint16_t cnt = 0;
+ble_char_t* character = NULL; 
+ble_char_t* character1 = NULL;
+ble_char_t* character2 = NULL;
+ble_descrp_t* description = NULL;
+static volatile uint32_t cnt = 0;
 uint8_t char1_str[] = "First Char";
-uint8_t desValue[] = "Notification Disable";
+uint8_t *desValue = NULL;
 uint8_t char1_str1[] = "Second Char";
 bool isConnected = false;
+uint8_t ni_key = false;  // notification and indication key
+bool isConfirm_needed = false;
 
 void notificationTask(void* args)
 {
-	while (true)
-	{
+	while (true){
 		cnt++;
-		vTaskDelay(2000 / portTICK_PERIOD_MS);
+		if(description){
+			
+			if(ni_key == 1)
+				esp_ble_gatts_send_indicate(profile->mGatt_if, profile->mConn_id, character->mhandle, sizeof(cnt), (uint8_t*)&cnt, isConfirm_needed);
+			else if(ni_key == 2)
+				esp_ble_gatts_send_indicate(profile->mGatt_if, profile->mConn_id, character->mhandle, sizeof(cnt), (uint8_t*)&cnt, isConfirm_needed);
+		}
+		vTaskDelay(500 / portTICK_PERIOD_MS);
 	}
-	
 }
 
 // void belDevice_events(ble_event_t e)
 // {
-//     switch (e._id)
+//     switcharacter (e._id)
 //     {
 //     case ble_device_connect:
 //         ESP_LOGI(TAG, "ble_device_connect\n");
@@ -64,40 +73,24 @@ void notificationTask(void* args)
 //     }
 // }
 
-void desWriteCallback(uint8_t* value, uint16_t len)
+void descrp_write_handler(esp_ble_gatts_cb_param_t* param)
 {
-	ESP_LOGI(TAG, "%s\n", __func__);
-	uint16_t descr_value= value[1]<<8 | value[0];
-	if(descr_value == 0x0001)
-	{
-		ESP_LOGI(TAG, "notify enable\n");
-		//the size of notify_data[] need less than MTU size
-		esp_ble_gatts_send_indicate(profile->mGatt_if, profile->mConn_id,  
-									des->mAssociate_char_handle,  
-									sizeof(cnt),  
-									(uint8_t*)&cnt, false);
+	uint16_t descr_value= param->write.value[1]<<8 | param->write.value[0];
+	if(descr_value == 0x0001){ // enable notification
+		ni_key = 1;
+		isConfirm_needed = false;
 	}
-	else if (descr_value == 0x0002)
-	{
-		ESP_LOGI(TAG, "indicate enable\n");
-		esp_ble_gatts_send_indicate(profile->mGatt_if, profile->mConn_id,  
-									des->mAssociate_char_handle,  
-									sizeof(cnt),  
-									(uint8_t*)&cnt, true);
+	else if (descr_value == 0x0002){ // enable indication
+		ni_key = 2;
+		isConfirm_needed = true;
 	}
-	else if (descr_value == 0x0000)
-	{
-		ESP_LOGI(TAG, "notify/indicate disable\n");
-	}	
-	else
-		ESP_LOGE(TAG, "unknown value\n");
+	else if (descr_value == 0x0000) // disable indication or notification.
+		ni_key = 0;
 }
 
 
-void app_main(void)
-{
-    printf("%s\r\n", CONFIG_IDF_TARGET);
-    printf("%s\r\n", CONFIG_BLE_CONN_NAME);
+void app_main(void){
+
     esp_err_t ret;
 	/* Initialize NVS. */
 	ret = nvs_flash_init();
@@ -113,30 +106,38 @@ void app_main(void)
 	// create default profile
 	profile = BleProfiles_createProfile();
 	BleDevice_addProfile(profile);
-	ble_service_t* service =  BleProfiles_createService((uint8_t*)&SERVICE_UUID, ESP_UUID_LEN_16, 0x06, true);
+	ble_service_t* service =  BleProfiles_createService((uint8_t*)&SERVICE_UUID, ESP_UUID_LEN_16, 0x08, true);
 	BleProfiles_addService(profile, service);
 
-    ch = BleProfiles_createCharacteristic((uint8_t*)&CHAR_UUID, ESP_UUID_LEN_16, ESP_GATT_AUTO_RSP);
-	BleProfiles_setCharacteristicPermission(ch, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE );
-	BleProfiles_setCharacteristicProperty(ch, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_WRITE | ESP_GATT_CHAR_PROP_BIT_NOTIFY );
-	BleProfile_setCharacteristicValue(ch, (uint8_t*)&cnt, sizeof(cnt), 0x40);
+    character = BleProfiles_createCharacteristic((uint8_t*)&CHAR_UUID, ESP_UUID_LEN_16, ESP_GATT_AUTO_RSP);
+	BleProfiles_setCharacteristicPermission(character, ESP_GATT_PERM_READ );
+	BleProfiles_setCharacteristicProperty(character, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_WRITE | ESP_GATT_CHAR_PROP_BIT_NOTIFY );
+	BleProfile_setCharacteristicValue(character, (uint8_t*)&cnt, sizeof(cnt), 0x40);
 	//
-	ch1 = BleProfiles_createCharacteristic((uint8_t*)&CHAR_UUID1, ESP_UUID_LEN_16, ESP_GATT_AUTO_RSP );
-	BleProfiles_setCharacteristicPermission(ch1, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE );
-	BleProfiles_setCharacteristicProperty(ch1, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_WRITE | ESP_GATT_CHAR_PROP_BIT_NOTIFY );
-	BleProfile_setCharacteristicValue(ch1, char1_str1, sizeof(char1_str1), 0x40);
+	character1 = BleProfiles_createCharacteristic((uint8_t*)&CHAR_UUID1, ESP_UUID_LEN_16, ESP_GATT_AUTO_RSP );
+	BleProfiles_setCharacteristicPermission(character1, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE );
+	BleProfiles_setCharacteristicProperty(character1, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_WRITE );
+	BleProfile_setCharacteristicValue(character1, char1_str1, sizeof(char1_str1), 0x40);
+
+	character2 = BleProfiles_createCharacteristic((uint8_t*)&CHAR_UUID2, ESP_UUID_LEN_16, ESP_GATT_AUTO_RSP );
+	BleProfiles_setCharacteristicPermission(character2, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE );
+	BleProfiles_setCharacteristicProperty(character2, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_WRITE );
+	BleProfile_setCharacteristicValue(character2, char1_str, sizeof(char1_str), 0x40);
 	
 	//
 
-    des = BleProfiles_createDescription((uint8_t*)&DES_UUID, ESP_UUID_LEN_16, ESP_GATT_AUTO_RSP);
-	BleProfiles_setDescriptionPermission(des,  ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE );
-	BleProfile_setDescriptionValue(des, (uint8_t*)&desValue, sizeof(desValue), 0x40);
-	des->mWriteEvent = desWriteCallback;
+    description = BleProfiles_createDescription((uint8_t*)&DES_UUID, ESP_UUID_LEN_16, ESP_GATT_AUTO_RSP);
+	BleProfiles_setDescriptionPermission(description,  ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE );
+	BleProfile_setDescriptionValue(description, (uint8_t*)&desValue, 0 , 0x40);
+	description->mWriteEvent = descrp_write_handler;
 	
 
-	BleProfiles_addCharacteristic(service, ch);
-    BleProfiles_addDescription(ch,des);
-	BleProfiles_addCharacteristic(service, ch1);
+	BleProfiles_addCharacteristic(service, character);
+    BleProfiles_addDescription(character,description);
+	BleProfiles_addCharacteristic(service, character1);
+	BleProfiles_addCharacteristic(service, character2);
 	BleDevice_activateProfiles();
 	xTaskCreate(notificationTask, "notificationTask", 1024 * 4, NULL, 5, NULL);
 }
+
+
